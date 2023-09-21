3c122f225ea5e71720bf80a1d4fb7930ff1a2bf0
Artem Tarasenko <shabashab.04@gmail.com>
2023-09-21 13:44:17 +0300

    feat(lab1): install vite plugins for auto import

--- lab1/vite.config.ts
+++ lab1/vite.config.ts
@@ -6,5 +6,35 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: 'src/components.d.ts',
+      directoryAsNamespace: true,
+    }),
+    AutoImport({
+      dts: 'src/auto-import.d.ts',
+      imports: [
+        'vue',
+        '@vueuse/head',
+        '@vueuse/core',
+        'vue-i18n',
+        'vue-router',
+        'pinia',
+      ],
+      dirs: [
+        './src/composable/*',
+        './src/store/*',
+        './src/models/**/*',
+        './src/api/*',
+      ],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+  ],
 })
