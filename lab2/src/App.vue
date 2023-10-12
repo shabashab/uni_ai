5c6dbbfac91f1b0a20f0dc3a588c3fa222fc235a
x0lotl <danil200319771@gmail.com>
2023-10-12 21:52:06 +0300

    feat(lab02): return input for dialog

--- lab2/src/App.vue
+++ lab2/src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-const { messages } = useChat();
+const { messages, sendMessage } = useChat();
 
 const newMessage = ref<string>("");
 
-const onGenerateButtonClick = () => {}
-
+const onNewMessageFromUserSent = () => {
+  sendMessage(newMessage.value);
+  newMessage.value = "";
+}
 </script>
 
 <template>
   <section id="chat_bg" class="w-full h-screen back-ground-gradient py-20">
     <div class="container bg-black/20 rounded-2xl py-4 px-8 text-white h-full flex flex-col justify-between gap-10">
       <ChatSection :messages="messages" />
-      <InputSection v-model:new-message="newMessage" @submit="onGenerateButtonClick()" />
+      <InputSection v-model:new-message="newMessage" @submit="onNewMessageFromUserSent" />
     </div>
   </section>
 </template>
@@ -19,6 +21,6 @@ const onGenerateButtonClick = () => {}
 <style scoped>
 .back-ground-gradient {
   background: rgb(229, 121, 231);
-  background: linear-gradient(14deg, rgb(97, 43, 98) 0%, rgb(11, 13, 141) 45%, rgba(38, 4, 54, 1) 71%);
+  background: linear-gradient(58deg, rgb(91, 196, 135) 0%, rgb(37, 159, 193) 45%, rgb(209, 34, 168) 71%);
 }
 </style>
--- lab2/src/components/InputSection.vue
+++ lab2/src/components/InputSection.vue
@@ -1,20 +1,34 @@
 <script setup lang="ts">
+import { Icon } from "@iconify/vue";
 
-const emit = defineEmits(["submit"]);
+const props = defineProps<{
+  newMessage: string;
+}>();
+
+const emit = defineEmits(["update:newMessage", "submit"]);
+
+const newMessageValue = computed({
+  get() {
+    return props.newMessage
+  },
+  set(value) {
+    emit("update:newMessage", value)
+  }
+})
 </script>
 
 <template>
-  <div class="h-20 flex justify-center gap-5">
-    <!-- <input 
+  <div class="h-20 flex justify-between gap-5">
+    <input 
       class="bg-neutral-900/70 w-full h-full text-xl px-8 rounded-2xl"
       v-model="newMessageValue"
       @keyup.enter="emit('submit')"
-    > -->
+    >
     <button
-      class="bg-green-500/90 rounded-xl w-full flex items-center justify-center text-3xl font-bold hover:cursor-pointer"
+      class="bg-green-500/90 rounded-xl w-20 flex items-center justify-center text-3xl font-bold hover:cursor-pointer"
       @click="emit('submit')"
     >
-      GENERATE
+      <Icon icon="tabler:send"></Icon>
     </button>
   </div>
 </template>
